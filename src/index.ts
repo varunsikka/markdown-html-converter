7c988ac727bc8bf361413505ca6298064df6ea17
Varun Sikka <vsikka@nestwealth.com>
2020-10-04 10:18:57 -0700

    feat: basic setup of the repository and the application

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { markdownToHtmlConverter } from './markdown-to-html';
---
+++ src/markdown-to-html/index.ts
@@ -0,0 +1,6 @@
+
+export * as Markdowns from './markdowns/index';
+
+export function markdownToHtmlConverter(input) {
+  return 'this is a converted markdown to html';
+}
